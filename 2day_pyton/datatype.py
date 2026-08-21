192816611dd3db7f97ecb3c2b017e67ba3b3c55c
ve-rshivam <sk0737877@gmail.com>
2026-08-21 22:09:40 +0530

    data type of variable

---
+++ 2day_pyton/datatype.py
@@ -0,0 +1,27 @@
+#Check the data type of all your variables using type() built-in function
+first_name = 'Shivam'
+middle_name = 'Kumar'
+last_name = 'Singh'
+country = 'India'
+city = 'Delhi'
+age = 19
+year = 2026
+is_grad = False
+is_light_on = True
+
+# multiple line variable
+first, last, count, age_of, is_light_off ='Shivam', 'Kumar', 'India', 19, 'False'
+print(type(first_name))
+print(type(middle_name))
+print(type(last_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_grad))
+print(type(is_light_on))
+print(type(first))
+print(type(last))
+print(type(count))
+print(type(age_of))
+print(type(is_light_off))
