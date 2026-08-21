e262c501d5eb206299b2875c0927419f7c530304
ve-rshivam <sk0737877@gmail.com>
2026-08-21 22:33:36 +0530

    variable operator

---
+++ 2day_pyton/variable_operation.py
@@ -0,0 +1,24 @@
+# Declare 5 as num_one and 4 as num_two
+# Add num_one and num_two and assign the value to a variable total
+# Subtract num_two from num_one and assign the value to a variable diff
+# Multiply num_two and num_one and assign the value to a variable product
+# Divide num_one by num_two and assign the value to a variable division
+# Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+# Calculate num_one to the power of num_two and assign the value to a variable exp
+# Find floor division of num_one by num_two and assign the value to a variable floor_division
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+print("Addition:",total)
+print("Subtraction:",diff)
+print("Multiplication:",product)
+print("Division:",division)
+print("Modulus:",remainder)
+print("Exponential:",exp)
+print("Floor Division Operator:",floor_division)
