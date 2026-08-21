b9e5f75951c4ac963ea2f63395fa3525546af477
ve-rshivam <sk0737877@gmail.com>
2026-08-21 15:14:46 +0530

    data type opration

---
+++ 1day_python/data_type.py
@@ -0,0 +1,12 @@
+print(type(10))                                 # integer
+print(type(11.34))                              # float     
+print(type(3.14))                               # float
+print(type(4 - 4j))                             # complex
+print(type(['Shivam', 'Python', 'India']))      # list
+print(type('Shivam'))                           # string
+print(type({'JS' : 'Atom', 'CS' : 'VS','Python' : ['Pycharm', 'Sublime'], 'JAVA' : {'JSE' : 'Netbeans', 'JEE' : 'Eclipse'}}))     #  dictionary
+print(type({25, 14 ,98 , 63, 75, 98}))          # set
+print(type(True))                               # boolean
+print(type(False))                              # boolean
+print(type((21, 36, 14, 25)))                   # tuple
+print(type(None))                               # None type 
