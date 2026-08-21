f1786fbdb1748e88d9fa626240dd8f766182e28c
ve-rshivam <sk0737877@gmail.com>
2026-08-21 21:58:12 +0530

    Variable naming

---
+++ 2day_pyton/variable.py
@@ -0,0 +1,27 @@
+first_name = 'Shivam'
+print('First Name: ',first_name)
+middle_name = 'Kumar'
+print('Middle Name: ',middle_name)
+last_name = 'Singh'
+print('Last Name: ',last_name)
+country = 'India'
+print('Country: ',country)
+city = 'Delhi'
+print('City: ',city)
+age = 19
+print('Age: ',age)
+year = 2026
+print('Year: ',year)
+is_grad = False
+print('Graduated:',is_grad)
+is_light_on = True
+print('Light On: ',is_light_on)
+
+# multiple line variable
+first, last, count, age_of, is_light_off ='Shivam', 'Kumar', 'India', '19', 'False'
+print(first, last, count, age_of, is_light_off)
+print('First Name: ',first)
+print('Last Name: ',last)
+print('Country: ',count)
+print('Age: ',age_of)
+print('Light off: ',is_light_off)
