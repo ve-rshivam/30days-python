8df9754ee185cc01b0d8b59a76be6b86e242fb3e
ve-rshivam <sk0737877@gmail.com>
2026-08-21 14:57:59 +0530

    arthematic opration

---
+++ 1day_python/arthematic.py
@@ -0,0 +1,18 @@
+x = int(input("Enter 1st number: "))
+y = int(input("Enter 2nd number: "))
+
+# Arthematic Operation:
+sum = x + y                     # addition
+sub = x - y                     # subtraction
+multiplication = x * y          # multiplication
+div = x / y                     # division
+mod = x % y                     # modulus
+expo = x ** y                   # exponential
+floor = x // y                  # floor division operator
+print("Addition:",sum)
+print("Subtraction:",sub)
+print("Multiplication:",multiplication)
+print("Division:",div)
+print("Modulus:",mod)
+print("Exponential:",expo)
+print("Floor Division Operator:",floor)  # Floor division (//) drops the decimal and returns an integer
