4a3578a52ee968794d3018615d354848b8dc117a
ve-rshivam <sk0737877@gmail.com>
2026-08-21 22:53:01 +0530

    circle operation

---
+++ 2day_pyton/circle_radius.py
@@ -0,0 +1,14 @@
+# The radius of a circle is 30 meters.
+# Calculate the area of a circle and assign the value to a variable name of area_of_circle
+# Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+# Take radius as user input and calculate the area.
+radius = 30
+area_of_circle = 3.14 * (radius ** 2)
+circum_of_circle = 2 * 3.14 * radius
+print('Area of circle:',area_of_circle)
+print('Circumference of a circle:',circum_of_circle)
+radius = int(input('Enter radius:'))
+area_of_circle = 3.14 * (radius ** 2)
+circum_of_circle = 2 * 3.14 * radius
+print('Area of circle:',area_of_circle)
+print('Circumference of a circle:',circum_of_circle)
