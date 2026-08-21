6f6d3712d449ae15927f18d133f5e3a80af508eb
ve-rshivam <sk0737877@gmail.com>
2026-08-21 22:16:06 +0530

    use in built length function

---
+++ 2day_pyton/length.py
@@ -0,0 +1,6 @@
+first_name = 'Shivam'
+last_name = 'Kumar'
+full_name = 'Shivam Kumar'
+print(len(first_name))
+print(len(last_name))
+print(len(full_name))       # when we use len function it also count space betwen two characters
