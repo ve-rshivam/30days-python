c06d1956ad78482c0e060089b8353787eccfd21d
ve-rshivam <sk0737877@gmail.com>
2026-08-23 11:28:28 +0530

    salary calculator per hour rate

---
+++ perhour_salary.py
@@ -0,0 +1,8 @@
+# Write a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+
+hours = float(input("Enter the workhour: "))
+rate = float(input("Enter rate per hour: "))
+
+salary = hours * rate
+
+print("Here is salary: ",salary)
