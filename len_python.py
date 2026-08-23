8aa4b0be7298ab33528708d006437918ab2727bc
ve-rshivam <sk0737877@gmail.com>
2026-08-23 10:53:49 +0530

    converting length in int to float to str

---
+++ len_python.py
@@ -0,0 +1,12 @@
+#Find the length of the text python and convert the value to float and convert it to string
+
+word = len('python')
+
+print(f"Length of 'python' word is {word}.")
+
+con_float = float(word)
+print(f"Length of 'python' word is {con_float} in float.")
+
+string = str(con_float)
+str1 = type(string)
+print(f"Here is its string {string} and its type is {str1}.")
