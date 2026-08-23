9d8baabf8cae3da1c2876d08279e3c79b67ba29d
ve-rshivam <sk0737877@gmail.com>
2026-08-23 09:50:59 +0530

    euclidean slope and distance

--- 3day_python/euclidean_slope.py
+++ 3day_python/euclidean_slope.py
@@ -7,5 +7,9 @@ x2 = 6
 y2 = 10
 
 slope = (y2 - y1) / (x2 - x1)
+dis1 = (x2 - x1) ** 2
+dis2 = (y2 - y1) ** 2
+distance = (dis1 + dis2) ** 0.5
 
 print('Slope between point (2, 2) and point (6,10): ',slope)
+print('Euclidean distance between point (2, 2) and point (6,10): ',distance)
