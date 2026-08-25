321dcc52d40c6ed453042d2fc78f17726fb812eb
ve-rshivam <sk0737877@gmail.com>
2026-08-25 07:42:04 +0530

    marks

---
+++ assignment/short_hand.py
@@ -0,0 +1,6 @@
+# Rewrite an if-else check for mark >= 50 (Pass/Fail) into a single-line ternary operator.
+marks = int(input("Enter your marks: "))
+if(marks >= 50):
+    print("Pass")
+else:
+    print("Failed")    
