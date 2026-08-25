b90d8ddf469ba24dc8112cef3d7270bf4271a210
ve-rshivam <sk0737877@gmail.com>
2026-08-25 07:25:49 +0530

    logical operator

---
+++ assignment/logical.py
@@ -0,0 +1,4 @@
+# Predict output of expression: (5 > 3) and (10 < 20 or 2 == 3). Verify in Python.
+
+print(5 > 3)                                # True
+print(10 < 20 or 2 == 3)                    # True
