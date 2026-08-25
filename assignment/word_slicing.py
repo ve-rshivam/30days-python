05e6c1a852954a8d1831de73b9711ef8e594494c
ve-rshivam <sk0737877@gmail.com>
2026-08-25 07:09:22 +0530

    word slicing

---
+++ assignment/word_slicing.py
@@ -0,0 +1,6 @@
+# Given str = "Python Programming", extract the word "Python" using positive slicing.
+
+given_str = "Python Programming"
+word = given_str [0:6]              # we can also write like this word = given_str [:6]
+
+print(word)
