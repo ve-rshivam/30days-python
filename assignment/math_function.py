5bcd3c4ab8e7826c5041a4d1401899fe4bb20db8
ve-rshivam <sk0737877@gmail.com>
2026-08-25 07:56:07 +0530

    maths function

---
+++ assignment/math_function.py
@@ -0,0 +1,9 @@
+# Write a function math_ops(a, b) returning addition, subtraction, and multiplication results together.
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+def math_ops():
+    print(a + b)
+    print(a - b)
+    print(a * b)
+math_ops()    
