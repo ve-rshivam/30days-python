2da0a86b26c5583ea32203437ec4b1bff765b4c4
ve-rshivam <sk0737877@gmail.com>
2026-08-25 07:23:32 +0530

    operator

---
+++ assignment/operator.py
@@ -0,0 +1,11 @@
+# Calculate and print the results of 17 / 4, 17 // 4, and 17 % 4. Explain differences.
+
+num1 = 17
+num2 = 4
+div = num1 / num2
+floor = num1 // num2
+remainder = num1 % num2
+
+print(div)
+print(floor)
+print(remainder)
