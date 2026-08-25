7088868e4f7fd3c707fe26c82daf89431d4557a0
ve-rshivam <sk0737877@gmail.com>
2026-08-25 07:19:51 +0530

    space removing and uppercase

---
+++ assignment/string_method.py
@@ -0,0 +1,9 @@
+# Take string " learning python ", remove leading/trailing spaces, and convert to uppercase.
+
+call = " learning python "
+#SPACE REMOVING
+print(call.strip())
+re_space = call.strip()
+
+# UPPERCASE/
+print(re_space.swapcase())
