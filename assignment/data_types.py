e15480c526dcb919a1e3c7b072cf7d3377785e49
ve-rshivam <sk0737877@gmail.com>
2026-08-25 07:01:31 +0530

    data types

---
+++ assignment/data_types.py
@@ -0,0 +1,11 @@
+# Write a script using type() to print data types of 5, 3.14, "Hello", and True.
+
+num1 = 5
+num2 = 3.14
+word = "Hello"
+state = True
+
+print(type(num1))
+print(type(num2))
+print(type(word))
+print(type(state))
