9f60cb0b2caf734738887750abeb74b6fd1fe4a4
ve-rshivam <sk0737877@gmail.com>
2026-08-25 06:58:38 +0530

    multi_assignment

---
+++ assignment/multi_assignment.py
@@ -0,0 +1,12 @@
+# Unpack the list colors = ["red", "green", "blue"] into variables r, g, and b in one statement.
+
+colors = ["red", "green", "blue"]
+r = colors[0]
+print(r)
+g = colors[1]
+print(g)
+b = colors[2]
+print(b)
+space = ' '
+statement = r + space + space + g + space + b
+print(statement)
