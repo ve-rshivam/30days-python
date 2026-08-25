064ce910aa425ad76da564fc7d6ccb5a7faaa4db
ve-rshivam <sk0737877@gmail.com>
2026-08-25 07:04:36 +0530

    type casting

---
+++ assignment/casting.py
@@ -0,0 +1,11 @@
+# Convert the float 9.8 to an integer and string "123" to an integer. Print both results.
+num1 = 9.8
+num2 = int(num1)
+word = "123"
+num3 = int(word)
+
+print(num2)
+print(num3)
+
+print(type(num2))
+print(type(num3))
