62042d6350f891efa9c577a8c2743f80c77e2429
ve-rshivam <sk0737877@gmail.com>
2026-08-26 20:40:54 +0530

    tab escape sequence

---
+++ 4day_python/tab_escape.py
@@ -0,0 +1,6 @@
+# Use a tab escape sequence to write the following lines.
+# Name      Age     Country   City
+# Shivam    19      India     Delhi  
+
+print("Name\tAge\tCountry\tCity")           # by using \t we can simply create a tab between two words which is generally equal to 4 space
+print("Shivam\t19\tIndia\tDelhi")
