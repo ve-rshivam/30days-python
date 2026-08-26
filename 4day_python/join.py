7c52c9d5e32edad8684d7226b5582c534ad07fb9
ve-rshivam <sk0737877@gmail.com>
2026-08-26 20:32:34 +0530

    join hash

---
+++ 4day_python/join.py
@@ -0,0 +1,8 @@
+# The following list contains the names of some of 
+# python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. 
+# Join the list with a hash with space string.
+
+python_libraries = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print('#'.join(python_libraries))
+
+# by using this we can print # after every word like Django#Flask#Bottle#Pyramid#Falcon    but it can't print in the last word
