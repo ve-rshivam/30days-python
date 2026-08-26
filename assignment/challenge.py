a9940345eb3c19e448781ffefd680bd0dc942afb
ve-rshivam <sk0737877@gmail.com>
2026-08-26 14:10:45 +0530

    even and odd

---
+++ assignment/challenge.py
@@ -0,0 +1,6 @@
+# Write a program to check whether a user-entered number is Even or Odd.
+num = int(input("Enter a number: "))
+if(num % 2 == 0):
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.") 
