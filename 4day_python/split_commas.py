934d8ea3bf490e7c0a549e77f7bf67252e8631ee
ve-rshivam <sk0737877@gmail.com>
2026-08-26 17:13:20 +0530

    split words at commas

---
+++ 4day_python/split_commas.py
@@ -0,0 +1,4 @@
+# "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+
+word = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(word.split(" ,"))
