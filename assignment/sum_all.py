7d4eb87fc2248af7d5003bc84350ed94f7123e33
ve-rshivam <sk0737877@gmail.com>
2026-08-26 14:07:37 +0530

    sum function

---
+++ assignment/sum_all.py
@@ -0,0 +1,9 @@
+# Create function sum_all(*numbers) that accepts any amount of numeric arguments and returns their sum.
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+def sum_all(num1, num2):
+    print(num1 + num2)
+    
+sum_all(num1, num2)
