43c91f8873963005e52f50c0e71ae6bb0c016c0b
ve-rshivam <sk0737877@gmail.com>
2026-08-26 20:35:50 +0530

    new line escape sequence

---
+++ 4day_python/escape.py
@@ -0,0 +1,6 @@
+# Use the new line escape sequence to separate the following sentences.
+# I am enjoying this challenge. I just wonder what is next.
+
+print('I am enjoying this challenge.\nI just wonder what is next.')
+
+# by using \n we can simply print anything cominjg after\n in new line.
