74443447e1798e4e51707425f427ba7347bb6334
ve-rshivam <sk0737877@gmail.com>
2026-08-26 20:16:14 +0530

    starting ending and removing space

---
+++ 4day_python/substring.py
@@ -0,0 +1,9 @@
+# Does 'Coding For All' start with a substring Coding?
+# Does 'Coding For All' end with a substring coding?
+# '   Coding For All      '  , remove the left and right trailing spaces in the given string.
+
+substring = 'Coding For All'
+
+print(substring.startswith('Coding'))           # it's check that string present in starting
+print(substring.endswith('Coding'))             # it's check that string present in last
+print(substring.strip())                        # it can remove all the space from starting and ending
