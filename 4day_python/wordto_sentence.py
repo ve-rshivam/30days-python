fbdd24ff17e36ecd8fc3523ef9006a64e112faf1
ve-rshivam <sk0737877@gmail.com>
2026-08-26 14:40:53 +0530

    word to sentence

---
+++ 4day_python/wordto_sentence.py
@@ -0,0 +1,8 @@
+# Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+word1 = 'Thirty'
+word2= 'Days'
+word3 = 'Of'
+word4 = 'Python'
+space = ' '
+sentence = word1 + space + word2 + space + word3 + space + word4
+print(sentence)
