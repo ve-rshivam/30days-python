a42492e71974bbb8d59a81f5494e35a5c5b73077
ve-rshivam <sk0737877@gmail.com>
2026-08-31 07:26:55 +0530

    getting first, mid and last item from list

---
+++ 5day_python/name_list.py
@@ -0,0 +1,9 @@
+# Get the first item, the middle item and the last item of the list
+name = ['Shivam','Kumar','Singh']
+first_item = name[0]
+middle_item = name[1]
+last_item = name[2]
+print(name)
+print(first_item)
+print(middle_item)
+print(last_item)
