ee7abbb03787abf016ebf40aa9eafa4bef3ff0da
ve-rshivam <sk0737877@gmail.com>
2026-08-31 07:22:16 +0530

    empty list and declare a list of more than 5 item

---
+++ 5day_python/empty_list.py
@@ -0,0 +1,9 @@
+# Declare an empty list
+empty_list = []
+
+# Declare a list with more than 5 items
+
+country = ['India', 'Nepal', 'Bhutan','Shri Lanka','Bangladesh']
+# Find the length of your list
+
+print(len(country))
