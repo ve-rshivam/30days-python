1c1ce3f31c878341e68c26a231106ef21db80b97
ve-rshivam <sk0737877@gmail.com>
2026-08-31 07:30:54 +0530

    mixed data types list

---
+++ 5day_python/mixed_data.py
@@ -0,0 +1,7 @@
+# Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixed_data_types = ['Shivam Kumar', 19, 5.11, False, 'India']
+print('Name: ',mixed_data_types[0])
+print('Age: ',mixed_data_types[1])
+print('Height: ',mixed_data_types[2])
+print('Marital Status: ',mixed_data_types[3])
+print('Address: ',mixed_data_types[4])
