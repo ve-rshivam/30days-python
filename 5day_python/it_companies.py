1e3d23fe763b72530404a8f21e74772b883dffd7
ve-rshivam <sk0737877@gmail.com>
2026-08-31 07:33:55 +0530

    it companies list

---
+++ 5day_python/it_companies.py
@@ -0,0 +1,5 @@
+# Declare a list variable named it_companies and assign initial values 
+# Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+
+it_companies  = ['Facebook','Microsoft','Apple','IBM','Oracle','Amazon']
+print(it_companies)
