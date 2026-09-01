3a57b7bca8f5464e143eca5fe6fa113406968744
ve-rshivam <sk0737877@gmail.com>
2026-09-01 20:42:37 +0530

    join two list

---
+++ 5day_python/join_list.py
@@ -0,0 +1,12 @@
+# Join the following lists:
+
+# front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+# back_end = ['Node','Express', 'MongoDB']
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node', 'Express', 'MongoDB']
+compiled_list = front_end + back_end
+print(compiled_list)
+
+# 2nd method
+front_end.append(back_end)
+print(front_end)
