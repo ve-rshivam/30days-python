af3e61a7d077001a40489125605ddd0f37e29382
ve-rshivam <sk0737877@gmail.com>
2026-09-04 21:11:10 +0530

    unpacking parent

---
+++ 6day/unpacking.py
@@ -0,0 +1,4 @@
+# Unpack siblings and parents from family_members
+family_member = ('brother1', 'brother2', 'brother', 'sister1', 'sister2', 'father', 'mother')
+parent = family_member[5:7]                     # upto 7 index here 7 is not include 
+print(parent)
