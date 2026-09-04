a339a48f6e7d4843d65d26a277ab10716a35e4b9
ve-rshivam <sk0737877@gmail.com>
2026-09-04 21:41:24 +0530

    nordic countries finding

---
+++ 6day/checking_countries.py
@@ -0,0 +1,9 @@
+# Check if 'Estonia' is a nordic country
+
+# Check if 'Iceland' is a nordic country
+
+# nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Estonia' in nordic_countries)
+print('Iceland' in nordic_countries)
