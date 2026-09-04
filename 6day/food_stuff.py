9e8e138b04d559d1d2e4cd2b2d7bb1c8fb53d726
ve-rshivam <sk0737877@gmail.com>
2026-09-04 21:22:44 +0530

    joining three tuples and assign in to new one

---
+++ 6day/food_stuff.py
@@ -0,0 +1,10 @@
+#Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+
+fruits = ('mango', 'apple', 'orange', 'lemon')
+vegetables = ('carrot', 'tamato', 'onion', 'potato')
+animal_product = ('product1', 'product2', 'product3')
+print(fruits)
+print(vegetables)
+print(animal_product)
+food_stuff_tp = fruits + vegetables + animal_product
+print(food_stuff_tp)
