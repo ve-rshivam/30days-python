70c39e7428cbe1a63b2441bd52c608ad1c7a675f
ve-rshivam <sk0737877@gmail.com>
2026-09-07 22:36:56 +0530

    different data types

---
+++ 7day/data_types.py
@@ -0,0 +1,23 @@
+
+# Explain the difference between the following data types: string, list, tuple and set
+#   STRING 
+# in string all the data store using single and double inverted commas it include all the characters and interger number in under '' and ""
+# LIST 
+# list is a data types which can have different types of data types like string, integer and floting 
+# list is intialization with square brackets and this is mutable(changeable)
+
+# TUPLE
+# tuple is a data types which can have different types of data types like string, integer and floting 
+# tuple is intialization with small brackets and this is unmutable(unchangeable) 
+
+# SET
+# set is a collection of item and it is a collection of unordered and unidexed distinct elements.
+# in python set is used to store unique items and 
+# it is possible to find it's union, intersection, difference, symmetric difference, subset, super set and
+# disjoint set among sets.
+# set never follows a sequence the value we entered we get in a random sequence 
+# if there are multiple same number exist in set then it is not necessary that we get that value same times that was 
+# entered at the time of intializaton
+
+# set usage a concept called hash and using hash we improve the performance we want to fetch elements as fast as possible
+# # in set indexing  is not supported 
