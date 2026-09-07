326fb4477f538ad7476fee0cd8ef15f4338fbba0
ve-rshivam <sk0737877@gmail.com>
2026-09-07 22:45:23 +0530

    get unique words using split and set

---
+++ 7day/unique.py
@@ -0,0 +1,6 @@
+# I am a teacher and I love to inspire and teach people. How many unique words have been used in the sentence? 
+# Use the split methods and set to get the unique words.
+sentence = 'I am a teacher and I love to inspire and teach people'
+set_sentence = set(sentence.split())
+print(set_sentence)
+print(len(set_sentence))
