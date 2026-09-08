2b4cd16380c28ff545d1fb872ba98ea866266c65
ve-rshivam <sk0737877@gmail.com>
2026-09-08 15:23:23 +0530

    creating square using loop

--- 8day/square_list.py
+++ 8day/square_list.py
@@ -2,3 +2,7 @@ squares =[]
 for x in range(10):
     squares.append(x ** 2)
 print(squares)    
+squares = list(map(lambda x: x**2, range(15)))
+print(squares)
+squares = [x**2 for x in range(13)]
+print(squares)
