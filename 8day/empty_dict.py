d60d3e86ede279201f5fc0c26fb67171e730bf4e
ve-rshivam <sk0737877@gmail.com>
2026-09-14 09:13:00 +0530

    empty ditonary

---
+++ 8day/empty_dict.py
@@ -0,0 +1,12 @@
+# Create an empty dictionary called dog
+dog = {}
+print(dog)
+print(type(dog))
+
+# Add name, color, breed, legs, age to the dog dictionary
+dog['name'] = 'Buddy'
+dog['color'] = 'dark brown'
+dog['breed'] = 'german'
+dog['legs'] = 'four'
+dog['age'] = 'three'
+print(dog)
