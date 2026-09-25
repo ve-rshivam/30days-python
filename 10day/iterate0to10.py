0f17adb3f9a31cc352ad0d188af4d8010ef248f7
ve-rshivam <sk0737877@gmail.com>
2026-09-25 20:39:29 +0530

    iterate forward and reverse using while and for loop

---
+++ 10day/iterate0to10.py
@@ -0,0 +1,17 @@
+# Iterate 0 to 10 using for loop, do the same using while loop.
+i = 0
+while i <= 10:
+    print(i)
+    i += 1
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in numbers:
+    print(number)   
+
+# Iterate 10 to 0 using for loop, do the same using while loop.
+i = 10
+while i >= 0:
+    print(i)
+    i -= 1
+numbers = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+for number in numbers:
+    print(number)    
